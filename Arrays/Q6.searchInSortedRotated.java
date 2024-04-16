5b0937d43d29c267e943d7e5f5fd1cc9618cfa41
Saurav Semwal <sauravsemwal@Sauravs-MacBook-Air.local>
2024-04-17 01:16:19 +0530

    rotated sorted search Q added

---
+++ Arrays/Q6.searchInSortedRotated.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] a, int target) {
+        int low = 0;
+        int high = a.length-1;
+        while (low<=high){
+            int mid = (low+high)/2;
+            if(a[mid]==target)
+                return mid;
+            // left part is sorted
+            if(a[mid]>=a[low]){
+                if(a[low]<=target && target<a[mid])
+                   high = mid-1;
+                else
+                    low = mid+1;
+            }
+            // right part is sorted
+            else{
+                if(a[mid]<target && target<=a[high])
+                    low = mid+1;
+                else
+                    high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
