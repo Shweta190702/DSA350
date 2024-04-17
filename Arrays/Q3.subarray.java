e7dd56f42dc55c7038e4eeef19631112eb3afb38
Saurav Semwal <sauravsemwal@Sauravs-MacBook-Air.local>
2024-04-18 02:10:55 +0530

    subarray printing

--- Arrays/Q3.subarray.java
+++ Arrays/Q3.subarray.java
@@ -3,6 +3,7 @@ class Solution {
     public int maxSubArray(int[] nums) {
         int sum=nums[0];
         int maxsum=nums[0];
+        int end = 0,start=0;
         for(int i=1;i<nums.length;i++){
             if(sum>=0){
                 sum+=nums[i];
@@ -11,9 +12,21 @@ class Solution {
             }
             if(sum>maxsum){
                 maxsum=sum;
+                end=i;
             }
             // sum=Math.max(sum+nums[i],nums[i]);
             // maxsum=Math.max(sum,maxsum);
+        }
+        int cummulativeSum=0;
+        for(int i=end;i>=0;i--){
+            cummulativeSum+=a[i];
+            if(cummulativeSum==maxSum){
+                start=i;
+                break;
+            }
+        }
+        for(int i=start;i<=end;i++){
+            System.out.println(a[i]);
         }
         return maxsum;
     }
