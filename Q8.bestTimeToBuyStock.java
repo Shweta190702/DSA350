1c197861bab4c5a49fe6f07430ad6db0d099090b
Saurav Semwal <sauravsemwal@Sauravs-MacBook-Air.local>
2024-04-17 23:09:20 +0530

    buy and sale stocks added

---
+++ Q8.bestTimeToBuyStock.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=prices[0];
+        int max= prices[0];
+        int answer=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]>max){ // if any element if greater than max then that can be a good time to sale stock
+                answer=Math.max((prices[i]-min),answer);
+                max=prices[i];
+            }
+            if(prices[i]<min){ // if any element if less than min then that can be a good time to buy stock
+                min=prices[i];
+                max=prices[i];
+            }
+        }
+        return answer;
+    }
+}
