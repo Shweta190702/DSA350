ff920d7a483393b80776dbf04bd1ca53ba0926fd
Saurav Semwal <sauravsemwal@Sauravs-MacBook-Air.local>
2024-04-18 02:09:45 +0530

    subarray printing

--- Arrays/Q3.subarray.java
+++ Arrays/Q3.subarray.java
@@ -4,17 +4,16 @@ class Solution {
         int sum=nums[0];
         int maxsum=nums[0];
         for(int i=1;i<nums.length;i++){
-            // if(sum>=0){
-            //     sum+=nums[i];
-            // }
-            // else{
-            //     sum=nums[i];
-            // }
-            // if(sum>maxsum){
-            //     maxsum=sum;
-            // }
-            sum=Math.max(sum+nums[i],nums[i]);
-            maxsum=Math.max(sum,maxsum);
+            if(sum>=0){
+                sum+=nums[i];
+            }else{
+                sum=nums[i];
+            }
+            if(sum>maxsum){
+                maxsum=sum;
+            }
+            // sum=Math.max(sum+nums[i],nums[i]);
+            // maxsum=Math.max(sum,maxsum);
         }
         return maxsum;
     }
